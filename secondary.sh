6de6874007e4bd21e64d075c0d28e9d78a5ed0c6
dmarkie <david.markie@otago.ac.nz>
2018-12-05 14:02:08 +1300

    Multiple changes

--- secondary.sh
+++ secondary.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
 
-# This line allows the option to only look for variants that are of 
-known phase only or variants that are both known and unknown phase
+# This line allows the option to only look for variants that are of known phase only or variants that are both known and unknown phase
 export includephase=unphased  # Options are phased or unphased
 
 
-# export 
-vcf=/mnt/hcs/WCHP_Clinical_Genetics/SequenceData/GRCh37/VariantCalls/Nexteraexome37Mb/20180518_Nextera37Mb_VariantCalls/20180518_Nextera37Mb_VariantCalls_Split_Annotated_4/20180518_Nextera37Mb_VariantCalls_Split_ann.vcf.gz 
+# Creates a variable that indicates the file that shows pedigree information for the cohort
+export ped=/mnt/hcs/WCHP_Clinical_Genetics/SequenceData/Meta/Ped.txt 
+
+# Creates a variable that indicates the file that has the information on the diseases that we are looking for
+export disorders=/resource/domains/STUDENT/leeto433/diseases3.txt 
+
 #Creates a vcf variable that indicates which vcf file to use
-export ped=/mnt/hcs/WCHP_Clinical_Genetics/SequenceData/Meta/Ped.txt # 
-Creates a variable that indicates the file that shows pedigree 
-information for the cohort
-export disorders=/resource/domains/STUDENT/leeto433/diseases3.txt # 
-Creates a variable that indicates the file that has the information on 
-the diseases that we are looking for
-
-
-export 
-vcf=/mnt/hcs/WCHP_Clinical_Genetics/SequenceData/GRCh37/VariantCalls/AV5UTRs/20180505_AV5UTRs_VariantCalls/20180505_AV5UTRs_VariantCalls_Split_Annotated_1/20180505_AV5UTRs_VariantCalls_Split_ann.vcf.gz
+export vcf=/mnt/hcs/WCHP_Clinical_Genetics/SequenceData/GRCh37/VariantCalls/AV5UTRs/20180505_AV5UTRs_VariantCalls/20180505_AV5UTRs_VariantCalls_Split_Annotated_1/20180505_AV5UTRs_VariantCalls_Split_ann.vcf.gz
 export samples="3075,3447,3690,3412"
-
-#export 
-vcf=/mnt/hcs/WCHP_Clinical_Genetics/SequenceData/GRCh37/VariantCalls/SeqCapEZ2/20180517_SeqCapEZ2_VariantCalls/20180517_SeqCapEZ2_VariantCalls_Split_Annotated_1/20180517_SeqCapEZ2_VariantCalls_Split_ann.vcf.gz
-#export samples="1203"
-
-export project=${SCRATCH}/$(basename ${vcf} .vcf.gz)_SecondaryFindings # 
-Creates a variable so that we can create a directory that is located in 
-the scratch folder of the person running the script, and removes the 
-.vcf.gz suffix and appends _SecondaryFindings onto the end of the 
-directory name
-mkdir -p ${project} # Creates the directory named as above. 'mkdir -p' 
-means make parent directories as needed
-cd $project # Go to the directory that was just created
-mkdir -p slurm # Create a directory for slurm output
-
-# specifies samples to analyse for secondary findings, set samples to 
-empty to analyse all the samples
+# export vcf=/mnt/hcs/WCHP_Clinical_Genetics/SequenceData/GRCh37/VariantCalls/SeqCapEZ2/20180517_SeqCapEZ2_VariantCalls/20180517_SeqCapEZ2_VariantCalls_Split_Annotated_1/20180517_SeqCapEZ2_VariantCalls_Split_ann.vcf.gz
+# export samples="1203"
+# export vcf=/mnt/hcs/WCHP_Clinical_Genetics/SequenceData/GRCh37/VariantCalls/Nexteraexome37Mb/20180518_Nextera37Mb_VariantCalls/20180518_Nextera37Mb_VariantCalls_Split_Annotated_4/20180518_Nextera37Mb_VariantCalls_Split_ann.vcf.gz 
 # export samples="2973" 
+
+# if the email variable has not been previously been specified, then try to find the email address for the the user from the /etc/slurm/userlist.txt file
+if [ -z ${email} ]; then
+	oldIFS=$IFS
+	IFS=$'\n'
+	userList=($(cat /etc/slurm/userlist.txt | grep $USER))
+	for entry in ${userList[@]}; do
+		testUser=$(echo $entry | awk -F':' '{print $1}')
+		if [ "$testUser" == "$USER" ]; then
+			export email=$(echo $entry | awk -F':' '{print $3}')
+			break
+		fi
+	done
+	IFS=$oldIFS
+	if [ -z ${email} ]; then
+		(echo "FAIL: Unable to locate email address for $USER in /etc/slurm/userlist.txt!" 1>&2)
+		exit 1
+	else
+		export MAIL_TYPE=REQUEUE,FAIL,END
+		(printf "%-22s%s (%s)\n" "Email address" "${email}" "$MAIL_TYPE" 1>&2)
+	fi
+fi
+if [[ ${email} != "none" ]]; then
+	mailme="--mail-user ${email} --mail-type $MAIL_TYPE"
+fi
+
+# Creates a variable so that we can create a directory that is located in the scratch folder of the person running the script, and removes the .vcf.gz suffix and appends _SecondaryFindings onto the end of the directory name
+export project=${SCRATCH}/$(basename ${vcf} .vcf.gz)_SecondaryFindings 
+# Creates the directory named as above. 'mkdir -p' means make parent directories as needed
+mkdir -p ${project} 
+# Go to the directory that was just created
+cd $project 
+# Create a directory for slurm output
+mkdir -p slurm 
+
+# specifies samples to analyse for secondary findings, set samples to empty to analyse all the samples
 if [[ ! -z ${samples ]]; then
 	echo ${samples} | tr "," "\n" > ${project}/selectedsamples.list
 	inputsamples=${project}/selectedsamples.list
@@ -44,17 +60,17 @@ else
 	inputsamples=${project}/allsamples.list	
 fi
 
-
-module purge # Clear any modules that may already have been loaded. Some 
-modules may interfere with BCFtools
-module load BCFtools # BCFtools is a program that allows us to work with 
-vcf files
-
-# If the allsamples.list file does not already exist, then create the 
-file which contains a list of the sample IDs. The -f flag tests whether 
-the file exists and is a regular file
+# Clear any modules that may already have been loaded. Some modules may interfere with BCFtools
+# BCFtools is a program that allows us to work with vcf files
+module purge 
+# BCFtools is a program that allows us to work with vcf files
+module load BCFtools
+# If the allsamples.list file does not already exist, then create the file which contains a list of the sample IDs. The -f flag tests whether the file exists and is a regular file
 if [ ! -f $project/allsamples.list ]; then
-	$(which bcftools) query -l ${vcf} > $project/allsamples.list # 
-query -l prints list of sample IDs only
+	# query -l in bcftools prints list of sample IDs only
+	$(which bcftools) query -l ${vcf} > ${project}/allsamples.list 
 fi
-
+# make an array of the sample IDs that will be processed
+export SAMPLEARRAY=($(cat ${inputsamples} | tr "\n" " "))
+# the number of entries in samplearray - should be the number of samples to be analysed
+export NUMSAMPLES=${#SAMPLEARRAY[@]}
